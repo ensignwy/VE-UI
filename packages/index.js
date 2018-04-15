f5fc1c0ef8b34ca8eb81cb7ef8ed0ae007c9188f
ensignwy <1403533052@qq.com>
2018-04-15 11:32:06 +0800

    入口

---
+++ packages/index.js
@@ -0,0 +1,20 @@
+import EScroller from './scroller/index.js';
+
+
+const components = [
+  EScroller
+]
+
+const install = function(Vue) {
+  if (install.installed) return
+  components.map(component => Vue.component(component.name, component))
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default {
+  install,
+  EScroller
+}
